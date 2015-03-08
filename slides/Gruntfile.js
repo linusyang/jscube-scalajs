7d6285e27555790644d07c09d68ab9385cba6b8d
Linus Yang <laokongzi@gmail.com>
2015-03-08 22:17:04 +0800

    Add slides

---
+++ slides/Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+  var config = {
+    clean: [
+        'index.html',
+        'ppt.js',
+        'ppt.css',
+        'fonts/'
+      ],
+    jade: {
+      src: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: '**/*.jade',
+          dest: './',
+          ext: '.html'
+        }]
+      }
+    },
+    stylus: {
+      src: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: '**/*.styl',
+          dest: './',
+          ext: '.css'
+        }],
+        options: {
+          compress: false
+        }
+      }
+    },
+    coffee: {
+      src: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: '**/*.coffee',
+          dest: './',
+          ext: '.js'
+        }]
+      }
+    },
+    copy: {
+      src: {
+        files: [{
+          expand: true,
+          cwd: 'src/fonts/',
+          src: '**/*',
+          dest: './fonts/'
+        },{
+          expand: true,
+          cwd: 'src/',
+          src: 'ppt.js',
+          dest: './'
+        }]
+      }
+    },
+    concurrent: {
+      compile: {
+        tasks: [
+          'jade',
+          'stylus',
+          'coffee',
+          'copy'
+        ],
+        options: {
+            logConcurrentOutput: false
+        }
+      },
+    },
+  };
+
+  grunt.initConfig(config);
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('default', ['clean', 'concurrent:compile']);
+
+};
---
+++ slides/README.md
@@ -0,0 +1,20 @@
+Scala.js Presentation
+=====
+
+A presentation for introducing [Scala.js](http://www.scala-js.org/) with theme from [rejectjs2013](https://github.com/mikemaccana/rejectjs2013/) by [@mikemaccana](https://github.com/mikemaccana). 
+
+Powered by [bespoke.js](http://markdalgleish.com/projects/bespoke.js).
+
+Build
+-----
+[io.js](http://iojs.org/) and [npm](https://www.npmjs.com/) are required for building from source.
+
+```bash
+git clone https://github.com/linusyang/jscube-scalajs.git
+cd jscube-scalajs/slides
+make
+```
+
+License
+-----
+Licensed under [Creative Commons BY-NC-SA 3.0](http://creativecommons.org/licenses/by-nc-sa/3.0/)
